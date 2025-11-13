3d4b21ead9864a37945a01d6533975794f042c66
Yūgen <michiel17jans@gmail.com>
2025-11-13 09:53:53 +0100

    Format
    * Changed the debug script code for better coherent gdscript formatting

--- a-3-game/scripts/debug.gd
+++ a-3-game/scripts/debug.gd
@@ -1,9 +1,10 @@
 extends Node
 
-func _unhandled_key_input(event : InputEvent) -> void:
-	var _event : InputEventKey = event
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	var _event: InputEventKey = event
 	if event.is_pressed():
-		var key : int = _event.keycode
+		var key: int = _event.keycode
 		match key:
 			KEY_R:
 				# Reset the level
@@ -11,6 +12,6 @@ func _unhandled_key_input(event : InputEvent) -> void:
 			KEY_Y:
 				get_tree().quit()
 			KEY_T:
-				var is_fullscreen : bool = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
-				var target_mode : int = DisplayServer.WINDOW_MODE_WINDOWED if is_fullscreen else DisplayServer.WINDOW_MODE_FULLSCREEN
+				var is_fullscreen: bool = DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN
+				var target_mode: int = DisplayServer.WINDOW_MODE_WINDOWED if is_fullscreen else DisplayServer.WINDOW_MODE_FULLSCREEN
 				DisplayServer.window_set_mode(target_mode)
