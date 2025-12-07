0ed28c83abc41a6b58031e8ed157aef92a6c4984
Yūgen <michiel17jans@gmail.com>
2025-12-07 10:33:25 +0100

    Fix
    * TileMapLayers now properly show inside the game window

--- a-3-game/scripts/UI/test_level.gd
+++ a-3-game/scripts/UI/test_level.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
 
+@onready var tile_map_layers: Node2D = $TileMapLayers
+@onready var tile_map_control: Node2D = $SubViewportContainer/SubViewport/TileMapControl
+
+
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color.BLACK)
+	
+	#move_child(tile_map_layers, sub_viewport_container.get_index())
+	#sub_viewport_container.move_child(get_child(sub_viewport_container.get_children().size()), 0)
+	tile_map_layers.reparent(tile_map_control)
