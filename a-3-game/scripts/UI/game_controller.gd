10766a6b5c1ee80b422672378ffebdaa57924e24
Yūgen <michiel17jans@gmail.com>
2025-12-08 00:18:21 +0100

    Bugfix
    * Game breaking moving platforms curve bug is now fixed

--- a-3-game/scripts/UI/game_controller.gd
+++ a-3-game/scripts/UI/game_controller.gd
@@ -1,10 +1,5 @@
 extends Node2D
 
 
-@onready var viewport_mover: Node2D = $ViewportMover
-@onready var sub_viewport: SubViewport = $SubViewportContainer/SubViewport
-
-
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color.BLACK)
-	viewport_mover.reparent(sub_viewport)
