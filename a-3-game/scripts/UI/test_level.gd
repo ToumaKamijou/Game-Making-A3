2bfe98d5959996903b78e244c0c3b4e4ebfe143d
Yūgen <michiel17jans@gmail.com>
2025-12-07 10:34:06 +0100

    Format
    * Deleted unnecessary code

--- a-3-game/scripts/UI/test_level.gd
+++ a-3-game/scripts/UI/test_level.gd
@@ -7,7 +7,4 @@ extends Node2D
 
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color.BLACK)
-	
-	#move_child(tile_map_layers, sub_viewport_container.get_index())
-	#sub_viewport_container.move_child(get_child(sub_viewport_container.get_children().size()), 0)
 	tile_map_layers.reparent(tile_map_control)
