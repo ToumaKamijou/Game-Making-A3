4589e4257c28a8e9a455484fd3904c6284e9052e
Yūgen <michiel17jans@gmail.com>
2025-12-07 11:10:41 +0100

    FIx
    * All the nodes now properly show outside the viewport

--- a-3-game/scripts/UI/test_level.gd
+++ a-3-game/scripts/UI/test_level.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 
-@onready var tile_map_layers: Node2D = $TileMapLayers
-@onready var tile_map_control: Node2D = $SubViewportContainer/SubViewport/TileMapControl
+@onready var sub_viewport_control: Node2D = $SubViewportControl
+@onready var sub_viewport: SubViewport = $SubViewportContainer/SubViewport
+
 
 
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color.BLACK)
-	tile_map_layers.reparent(tile_map_control)
+	sub_viewport_control.reparent(sub_viewport)
