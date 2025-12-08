f7f90c8b61af9c2b3d1da36570e188c3d22737d6
Yūgen <michiel17jans@gmail.com>
2025-12-08 12:02:54 +0100

    Final area

---
+++ a-3-game/scripts/2D/gameplay/final_wall.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+
+var player : Player
+
+
+func _ready() -> void:
+	for i in get_parent().get_parent().get_children(true):
+		if i.is_in_group("Player"):
+			player = i
+			return
+
+
+func _process(_delta: float) -> void:
+	if player and player.score >= 10:
+		queue_free()
