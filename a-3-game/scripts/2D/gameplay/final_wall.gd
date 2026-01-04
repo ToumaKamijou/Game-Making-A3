7c0fb933b05654b7fdd7f818e03ca199d1385200
Yūgen <michiel17jans@gmail.com>
2026-01-04 12:14:20 +0100

    Finished Level

--- a-3-game/scripts/2D/gameplay/final_wall.gd
+++ a-3-game/scripts/2D/gameplay/final_wall.gd
@@ -5,12 +5,12 @@ var player : Player
 
 
 func _ready() -> void:
-	for i in get_parent().get_parent().get_children(true):
+	for i in get_parent().get_parent().get_parent().get_children(true):
 		if i.is_in_group("Player"):
 			player = i
 			return
 
 
 func _process(_delta: float) -> void:
-	if player and player.score >= 10:
+	if player and player.score >= 5:
 		queue_free()
