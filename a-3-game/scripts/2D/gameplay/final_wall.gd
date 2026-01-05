076a48b40c78222936be1338398dbd8beb2225d3
Sankakutai <leeuwenjonas@gmail.com>
2026-01-05 21:35:18 +0100

    removed failsafes

--- a-3-game/scripts/2D/gameplay/final_wall.gd
+++ a-3-game/scripts/2D/gameplay/final_wall.gd
@@ -1,25 +1,6 @@
 extends StaticBody2D
 
-# None of these values are doing anything here, but it prevents crashes. This object should not have used the flashable_wall instance.
-@onready var raycast = $RayCast2D
-var laser_origin: Node2D
-var collider: Node2D
-
-var blocked := false
-
-var laser: Node2D = null
-var laser_color: int
-
-var player_lit := false
-var override := false
-var matched := false
-var just_lit := false
-var laser_original_position: Vector2
-
-@onready var mesh: MeshInstance2D = $MeshInstance2D
-
 var player : Player
-
 
 func _ready() -> void:
 	for i in get_parent().get_parent().get_parent().get_children(true):
