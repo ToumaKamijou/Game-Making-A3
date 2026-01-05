f7f4ca3de72ec5c43a0ad38a907e0d5a33813312
Yūgen <michiel17jans@gmail.com>
2026-01-05 01:39:48 +0100

    Feat
    * Added a basic minimap

---
+++ a-3-game/scripts/UI/minimap_viewport.gd
@@ -0,0 +1,13 @@
+extends SubViewport
+
+
+@onready var player: Player = $"../../../GameWorld/Player"
+
+@onready var camera_2d: Camera2D = $Camera2D
+
+
+#func _ready() -> void:
+	#world_2d = get_tree().root.world_2d
+
+func _process(_delta: float) -> void:
+	camera_2d.position = player.position
